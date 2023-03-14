9e574be7601b51f9c438a68d0e6e2f77843c6f6a
vanessarb1001 <127886686+vanessarb1001@users.noreply.github.com>
2023-03-14 14:22:56 -0400

    Add files via upload

---
+++ Cuenta.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace CuentaBancaria
+{
+	public class Cuenta
+	{
+		private string nombre;
+		private double balance;
+		
+		public Cuenta()
+		{
+			this.nombre="";
+			this.balance=0.00;
+		}
+		
+		public Cuenta(string nombre, double balance)
+		{
+			this.nombre=nombre;
+			this.balance=balance;
+		}
+		
+		
+		public string Nombre {
+			get {
+				return nombre;
+			}
+			set {
+				nombre = value;
+			}
+		}
+		
+		
+		public double Balance {
+			get {
+				return balance;
+			}
+			set {
+				balance = value;
+			}
+		}
+		
+		public void Depositar()
+		{
+			Console.Write("¿Cuanto desea ingresar en su cuenta? ");
+			double deposito = double.Parse(Console.ReadLine());
+			
+			Balance = Balance + deposito;
+			
+			Console.WriteLine("Ha ingresado " + deposito + " pesos en su cuenta");
+		}
+		
+		public void Retirar()
+		{
+			Console.Write("¿Cuanto desea retirar de su cuenta? ");
+			double retiro = double.Parse(Console.ReadLine());
+			
+			if (retiro <= Balance) 
+			{
+				Balance = Balance - retiro;
+				Console.WriteLine("El proceso de retiro ha sido un exito");
+			}
+			else
+			{
+				Console.WriteLine("Lo sentimos, no puede retirar más del saldo actual");
+			}
+		}
+		
+		
+	}
+}
---
+++ CuentaTester.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace CuentaBancaria
+{
+	class CuentaTester
+	{
+		public static void Main(string[] args)
+		{
+			int opcion=0;
+			
+			Cuenta banco = new Cuenta();
+			
+			Console.WriteLine("Ingrese su nombre por favor:");
+			banco.Nombre = Console.ReadLine();
+			
+			do
+			{
+				Console.Clear();
+				
+				Console.WriteLine("Bienvenido al sistema " +banco.Nombre);
+
+				Console.WriteLine("1- Para consultar Balance");
+				Console.WriteLine("2- Para hacer un deposito");
+				Console.WriteLine("3- Para hacer un retiro de cuenta");
+				Console.WriteLine("4- Para salir del sistema");
+				
+				Console.Write("Seleccione la opcion de us preferencia -> ");
+				opcion = int.Parse(Console.ReadLine());
+				
+				switch (opcion)
+				{
+					case 1:
+						if (banco.Balance >= 1.00)
+						{
+							Console.WriteLine("Su Balance actual es de: " +banco.Balance+ " pesos. ");
+						}
+						else
+						{
+							Console.WriteLine("Su cuenta está vacia");
+						}
+						break;
+					case 2:
+						Console.WriteLine("Ha elegido hacer deposito");
+						banco.Depositar();
+						break;
+					case 3:
+						Console.WriteLine("Ha decidido retirar");
+						banco.Retirar();
+						break;
+					case 4:
+						Console.WriteLine("Ha elegido salir");
+						break;
+					default:
+						Console.WriteLine("\n Opcion no valida!");
+						break;
+				}
+				
+				Console.WriteLine("Pulsa una tecla cualquiera para poder regresar al menú");
+				Console.ReadKey();
+				Console.Clear();
+				
+			} while(opcion != 4);
+			Console.WriteLine("Gracias por preferirnos, usuario " +banco.Nombre);
+			Console.ReadKey();
+		}
+	}
+}
